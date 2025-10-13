5b1c29df2ae1018da166537ea40d0c76a12a5d70
Mauriac <anatole.mauriac@gmail.com>
2025-10-13 14:47:37 +0200

    exo2

--- exo2.c
+++ exo2.c
@@ -2,14 +2,16 @@
 #include <math.h>
 
 int main(){
-    float m,c,t,n;
+    float m,c,t,t2,n;
 
     printf("quel est le montant du pret : ");
     scanf("%f",&c);
     printf("quel est le taux d'interet annuel : ");
-    scanf("%f",&t);
+    scanf("%f",&t2);
     printf("quel est la duree du pret en annees : ");
     scanf("%f",&n);
+
+    t=t2/100;
 
     m=(c*(t/12))/(1-(pow(1+(t/12),-n*12)));
     printf("m = %f",m);
