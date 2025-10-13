e7a204065e0e548f17799619e0ecf4b6d81b478c
Mauriac <anatole.mauriac@gmail.com>
2025-10-13 14:07:24 +0200

    exo1

--- exo1.c
+++ exo1.c
@@ -2,12 +2,12 @@
 
 int main(){
     int nb,s,m,h;
-    printf("choississez un nombre de seconde");
-    scanf("%d",nb);
+    printf("choississez un nombre de seconde: ");
+    scanf("%d",&nb);
 
-    h=nb%3600;
-    m=(nb-h)%60;
-    s=nb-h-m;
+    h=nb/3600;
+    m=(nb%3600)/60;
+    s=(nb%3600)%60;
 
     printf("%d secondes = %d heures, %d minutes et %d secondes.",nb,h,m,s);
     return 0;
