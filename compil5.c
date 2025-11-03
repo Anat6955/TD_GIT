166f776e4de9d5de765806bf4458fe26be0aab1e
Mauriac <anatole.mauriac@gmail.com>
2025-11-03 14:13:18 +0100

    compil5

--- compil5.c
+++ compil5.c
@@ -2,6 +2,7 @@
 
 int main() {
     char c = 'A';
-    printf("c = %s\n", c);
+    printf("c = %c\n", c);
     return 0;
 }
+/*Le probleme vient du fait que c est un charatere et que %s c'est pour les chaines de charactere*/
