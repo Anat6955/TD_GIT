262ee8a56f62ff148a729309c615381c1d6717d4
Mauriac <anatole.mauriac@gmail.com>
2025-11-03 15:17:03 +0100

    exec8

--- exec8.c
+++ exec8.c
@@ -2,7 +2,8 @@
 
 int main() {
     int t[3] = {10, 20, 30};
-    int i = -1;
+    int i = 2;
     printf("%d\n", t[i]); 
     return 0;
 }
+/*On peut pas utiliser -1 pour aller a la fin de la liste*/
