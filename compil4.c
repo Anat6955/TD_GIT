127edc93b451235411740ead23880d57bc4c6817
Mauriac <anatole.mauriac@gmail.com>
2025-11-03 14:06:39 +0100

    compil4

--- compil4.c
+++ compil4.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main {
+int main() {
     printf("Test\n");
     return 0;
 }
+/*compil4.c:3:10: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+ int main {
+          ^
+Le probleme vient du fait qu'il manque les parentheses apres le main*/
