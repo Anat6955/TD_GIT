792d4acd74892fa137a62fc0995450f356ec7d3f
Mauriac <anatole.mauriac@gmail.com>
2025-11-03 14:35:29 +0100

    compil11

--- compil11.c
+++ compil11.c
@@ -3,8 +3,16 @@
 int main() {
     if (1) {
         printf("ok\n");
-    } else
+    } 
+    else{
         printf("nope\n");
-        }
+    }
     return 0;
 }
+/*compil11.c:9:5: error: expected identifier or '(' before 'return'
+     return 0;
+     ^~~~~~
+compil11.c:10:1: error: expected identifier or '(' before '}' token
+ }
+ ^
+ Le probleme vient du fait qu'il manque quillemée apres le else*/
