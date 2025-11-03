f247709369e230678c148f7abf2b3741c90b9f52
Mauriac <anatole.mauriac@gmail.com>
2025-11-03 14:46:20 +0100

    exec4

--- exec4.c
+++ exec4.c
@@ -2,8 +2,9 @@
 
 int main() {
     int i = 0;
-    while (i < 10) {
+    while (i < 10, i++) {
         printf("%d\n", i);
     }
     return 0;
 }
+/*Il y a une boucle infinie car i < 10 est toujours vrai*/
