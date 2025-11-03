d67ec27c63ff3aaddeb27648f59e4a57b56ca20e
Mauriac <anatole.mauriac@gmail.com>
2025-11-03 14:44:16 +0100

    exec3

--- exec3.c
+++ exec3.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int x;
+    int x = 2;
     int y = x + 5;  
     printf("%d\n", y);
     return 0;
 }
+/*La variable y fait appelle a la variable x mais elle n'a pas de valeur*/
