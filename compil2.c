e182eaf75e1c8dbeba73db4db2f9ee573b20fa1e
Mauriac <anatole.mauriac@gmail.com>
2025-11-03 13:56:58 +0100

    compil2

--- compil2.c
+++ compil2.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int x = "42";
+    int x = 42;
     printf("%d\n", x);
     return 0;
 }
+/*compil2.c: In function 'main':
+compil2.c:4:13: warning: initialization makes integer from pointer without a cast [-Wint-conversion]
+     int x = "42";
+             ^~~~
+Le probleme vient du fait que la variable x est un entier est qu'il y a des parantheses autour du 42*/
