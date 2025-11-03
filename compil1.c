98b8853d0e8677129877ce0956f9a39d35be04bc
Mauriac <anatole.mauriac@gmail.com>
2025-11-03 13:47:56 +0100

    compil1

--- compil1.c
+++ compil1.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world\n")
+    printf("Hello world\n");
     return 0;
 }
+/*compil1.c: In function 'main':
+compil1.c:5:5: error: expected ';' before 'return'
+     return 0;
+     ^~~~~~
+
+Le probleme vient du fait qu'il manque le point virgule a la fin de la ligne printf("Hello world\n")*/
