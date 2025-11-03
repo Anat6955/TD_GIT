0833e2e950c8e9d578c358648657cbb3a327f7b0
Mauriac <anatole.mauriac@gmail.com>
2025-11-03 14:17:09 +0100

    compil6

--- compil6.c
+++ compil6.c
@@ -2,8 +2,9 @@
 
 int main() {
     int x = 10;
-    if (x = 5) {
+    if (x == 5) {
         printf("x vaut 5\n");
     }
     return 0;
 }
+/*Le probleme vient du fait que pour comparer il faut faire x==5 et pas x=5*/
