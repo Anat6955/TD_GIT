4c04927ed5c470e1188d9e9de34aa388c1d990fa
Mauriac <anatole.mauriac@gmail.com>
2025-11-03 14:40:18 +0100

    exec1

--- exec1.c
+++ exec1.c
@@ -2,8 +2,9 @@
 
 int main() {
     int a = 2;
-    int b = 0;
+    int b = 1;
     int c = a / b;  // crash / comportement indéfini
     printf("%d\n", c);
     return 0;
 }
+/*Ici on divise par b qui est egale a 0 (c'est pas possible)*/
