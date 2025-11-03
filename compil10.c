658d627ca1bd6303e66f197c41f9330d6787245a
Mauriac <anatole.mauriac@gmail.com>
2025-11-03 14:30:35 +0100

    compil9

--- compil10.c
+++ compil10.c
@@ -5,3 +5,12 @@ int main() {
     printf("%d\n", 1abc);
     return 0;
 }
+/*compil10.c: In function 'main':
+compil10.c:4:9: error: invalid suffix "abc" on integer constant
+     int 1abc = 5;
+         ^~~~
+compil10.c:4:9: error: expected identifier or '(' before numeric constant
+compil10.c:5:20: error: invalid suffix "abc" on integer constant
+     printf("%d\n", 1abc);
+                    ^~~~
+*/
