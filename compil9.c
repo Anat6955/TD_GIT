ee56d2359f519b0b44360612ed51a29c5d76c324
Mauriac <anatole.mauriac@gmail.com>
2025-11-03 14:28:40 +0100

    compil9

--- compil9.c
+++ compil9.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    char s[3] = "Hello";
+    char s[5] = "Hello";
     printf("%s\n", s);
     return 0;
 }
+/*compil9.c: In function 'main':
+compil9.c:4:17: warning: initializer-string for array of chars is too long
+     char s[3] = "Hello";
+                 ^~~~~~~
+Le probleme vient du fait que hello fait 5 characteres et la variable s a un max de 3 charactere*/
