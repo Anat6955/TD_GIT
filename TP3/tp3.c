612522ceaf4e52956af43e139d150f8a8c4999ef
Mauriac <anatole.mauriac@gmail.com>
2025-11-17 13:29:24 +0100

    Afficher le tableau comme une image + corection du printf

--- TP3/tp3.c
+++ TP3/tp3.c
@@ -5,7 +5,7 @@ int main(){
     int width = 0;
     int max_height = 0;
 
-    prints("Saisisse une taille n, 5 ≤ n ≤ 10 : ");
+    printf("Saisisse une taille de n entre 5 et 10 : ");
     scanf("%d",&n);
 
     width = n * n;
@@ -15,7 +15,15 @@ int main(){
 
     for (int i = 0; i< max_height; i++){
         for (int j = 0; j< width; j++){
-            img[i][j]=" ";
+            img[i][j]=' ';
         }
     }
+
+    for (int i = 0; i< max_height; i++){
+        for (int j = 0; j< width; j++){
+            printf("%c",img[i][j]);
+        }
+        printf("\n");
+    }
+
 }
