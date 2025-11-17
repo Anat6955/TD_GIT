6732dc12bbe00c358dd697cf47d653d0d5b778b4
Mauriac <anatole.mauriac@gmail.com>
2025-11-17 14:08:21 +0100

    Dessiner la pointe de l’étoile

--- TP3/tp3.c
+++ TP3/tp3.c
@@ -4,20 +4,41 @@ int main(){
     int n = 0;
     int width = 0;
     int max_height = 0;
+    int mid = 0;
+    char img[50][150];
 
     printf("Saisisse une taille de n entre 5 et 10 : ");
     scanf("%d",&n);
 
     width = n * n;
     max_height = 3 * n - 1; 
-
-    char img[150][50];
+    mid = width / 2;
+    printf("%d\n%d\n%d\n",width, max_height, mid);
 
     for (int i = 0; i< max_height; i++){
         for (int j = 0; j< width; j++){
             img[i][j]=' ';
         }
     }
+
+    for(int i=0; i<width;  i++){
+        img[n+1][i]='_';
+    }
+
+    for (int i = 0; i<= n+1; i++){
+        for (int j = 0; j<i-1; j++){
+            img[i][mid+j]='A';
+            img[i][mid-j]='A';
+        }  
+    }
+
+    for (int i = 0; i<= n; i++){
+        for (int j = 0; j<i-1; j++){
+            img[i+1][mid+j]='S';
+            img[i+1][mid-j]='S';
+        }  
+    }
+
 
     for (int i = 0; i< max_height; i++){
         for (int j = 0; j< width; j++){
