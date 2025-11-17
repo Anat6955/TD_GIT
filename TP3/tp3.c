1eed07395071eb973848ab8d16f85606fb0ecd2e
Mauriac <anatole.mauriac@gmail.com>
2025-11-17 13:08:35 +0100

    Dimensions de l’image

--- TP3/tp3.c
+++ TP3/tp3.c
@@ -2,7 +2,12 @@
 
 int main(){
     int n = 0;
+    int width = 0;
+    int max_height = 0;
 
     prints("Saisissé une taille n, 5 ≤ n ≤ 10 : ");
     scanf("%d",&n);
+
+    width = n * n;
+    max_height = 3 * n - 1; 
 }
