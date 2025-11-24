cbb70680f2bdcbfcf040e181e0c0b6bebd881b90
Mauriac <anatole.mauriac@gmail.com>
2025-11-24 13:59:50 +0100

    saisir notes

--- tp4/exo1.c
+++ tp4/exo1.c
@@ -15,7 +15,7 @@ void afficherMenu(){
 int lireChoix(){
     int choix = -1;
     printf("Choississer un nombre : ");
-    scanf("%d",&choix);
+    scanf("%d", &choix);
     return choix;
 }
 
@@ -23,7 +23,7 @@ int saisirNombreEleves(){
     int nbEleve = 0;
     while (nbEleve < 1 || nbEleve > 30){
         printf("Combien y a t il d eleve dans votre classe (1 a 30) : ");
-        scanf("%d",&nbEleve);
+        scanf("%d", &nbEleve);
         if (nbEleve < 1 || nbEleve > 30){
             printf("Valeur invalide.\n");
         }
@@ -31,11 +31,32 @@ int saisirNombreEleves(){
     return nbEleve;
 }
 
+char saisirNote(int nbEleve){
+    printf("nombre elees %d", nbEleve);
+    char noteEleve[nbEleve][3];
+    printf("Saisir les notes de 3 controles pour %d eleves.\n", nbEleve);
+    for (int i = 0; i < nbEleve; i++){
+        printf("Eleve %d :\n", i+1);
+        for (int j = 0; j < 3; j++){
+            int note = 0;
+            printf("Note du controle %d (0 a 20) : ", j+1);
+            scanf("%d", &note);
+            while (note < 0 || note > 20){
+                printf("Note invalide.\n");
+                printf("Note du controle %d (0 a 20) : ", j+1);
+                scanf("%d", &note);
+            }
+            noteEleve[i][j] = note;
+        }
+    }
+    return noteEleve[nbEleve][3];
+}
+
 int main(){
     afficherMenu();
     int choix=lireChoix();
     printf("Vous avez choisi : %d\n",choix);
     int nbEleve = saisirNombreEleves();
-
+    char noteEleve = saisirNote(nbEleve);
     return 0;
 }
