ba83407f35c29823fd91c2fc474368db0a605290
Mauriac <anatole.mauriac@gmail.com>
2025-11-25 18:38:26 +0100

    J'ai finni les swicht

--- tp4/exo1.c
+++ tp4/exo1.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-
 void afficherMenu(){
-    printf("==== GESTION DE NOTES ====\n");
+    printf("\n==== GESTION DE NOTES ====\n");
     printf("1. Saisir le nombre d eleves\n");
     printf("2. Saisir les notes des eleves\n");
     printf("3. Afficher toutes les notes\n");
     printf("4. Afficher la moyenne d'un eleve\n");
     printf("5. Afficher la moyenne generales\n");
     printf("6. Afficher la meilleur note de chaque controle\n");
-    printf("0. Quitter\n");
+    printf("0. Quitter\n\n");
 }
 
 int lireChoix(){
@@ -32,7 +31,7 @@ int saisirNombreEleves(){
 }
 
 int saisirNote(int nbEleve, int tabNoteEleve[30][3]) {
-    printf("nombre eleves %d", nbEleve);
+    printf("nombre eleves %d\n", nbEleve);
     printf("Saisir les notes de 3 controles pour %d eleves.\n", nbEleve);
     for (int i = 0; i < nbEleve; i++){
         printf("Eleve %d :\n", i+1);
@@ -91,33 +90,56 @@ int trouverMeilleureNoteControle(int tabNoteEleve[30][3], int indiceControle, in
     return meilleureNote;
 }
 
+void afficherMeilleuresNotes(int tabNoteEleve[30][3], int nbEleve){
+    for (int i = 0; i < 3; i++){
+        printf("Meilleur note au controle %d : %d\n",i+1,trouverMeilleureNoteControle(tabNoteEleve, i, nbEleve));
+    }
+}
+
 int main(){
-    afficherMenu();
-
-    int choix=lireChoix();
-    printf("Vous avez choisi : %d\n",choix);
-    int nbEleve = saisirNombreEleves();
-
+    int nbEleve = 0;
     int tabNoteEleve[30][3];
-    saisirNote(nbEleve,tabNoteEleve);
-
-    afficherNote(tabNoteEleve, nbEleve);
-
     int indiceEleve = 0;
-    printf("Choississer un eleve : ");
-    scanf("%d", &indiceEleve);
-    float moyenne = calculerMoyenneEleve(tabNoteEleve, indiceEleve);
-    printf("Sa moyenne est de : %f\n",moyenne);
 
-    float moynneGenerale = 0;
-    moynneGenerale = calculerMoyenneGenerale(tabNoteEleve, nbEleve);
-    printf("La moyenne generale est de : %f\n", moynneGenerale);
+    while (1){
+        afficherMenu();
 
-    int meilleureNoteControle = 0;
-    int indiceControle = 0;
-    printf("Choississer un controle d on vous voulez connaitre la meilleur note : ");
-    scanf("%d", &indiceControle);
-    meilleureNoteControle = trouverMeilleureNoteControle(tabNoteEleve, indiceControle, nbEleve);
-    printf("La meilleure note du controlle %d est : %d\n", indiceControle, meilleureNoteControle);
+        int choix = lireChoix();
+        printf("Vous avez choisi : %d\n", choix);
+
+        switch (choix)
+        {
+        case 1:
+            nbEleve = saisirNombreEleves();
+            break;
+
+        case 2:
+            saisirNote(nbEleve, tabNoteEleve);
+            break;
+
+        case 3:
+            afficherNote(tabNoteEleve, nbEleve);
+            break;
+
+        case 4:
+            printf("Choisissez un eleve : ");
+            scanf("%d", &indiceEleve);
+            printf("Sa moyenne est de : %f\n",
+            calculerMoyenneEleve(tabNoteEleve, indiceEleve));
+            break;
+
+        case 5:
+            printf("La moyenne generale est : %f\n",
+            calculerMoyenneGenerale(tabNoteEleve, nbEleve));
+            break;
+
+        case 6:
+            afficherMeilleuresNotes(tabNoteEleve, nbEleve);
+            break;
+
+        case 0:
+            return 0;
+        }
+    }
     return 0;
 }
