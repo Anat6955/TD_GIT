613b5a4772858b4c261e06f33699b97bce7870dd
Mauriac <anatole.mauriac@gmail.com>
2025-11-27 16:57:27 +0100

    bouvle afficher

--- tp4/exo2.c
+++ tp4/exo2.c
@@ -8,6 +8,17 @@ void afficher_menu(){
 
 
 int main (){
-    afficher_menu();
+    int choix = 1;
+    while(choix == 1){
+        afficher_menu();
+        printf("Votre choix : ");
+        scanf("%d",&choix);
+        if (choix != 1 || choix != 2){
+            printf("Choississez entre 1 et 2\n");
+            main();
+        }
+    }
+    
+
     return 0;
 }
