a383a46f74f9d33c3fe65e93759c977421705139
Mauriac <anatole.mauriac@gmail.com>
2025-11-27 17:10:32 +0100

    saisir montant plus if choix

--- tp4/exo2.c
+++ tp4/exo2.c
@@ -6,6 +6,19 @@ void afficher_menu(){
     printf("2 - Quitter\n");
 }
 
+int saisir_montant(){
+    int montant = 0;
+    printf("Montant a retirer (il doit etre compris entre 5 est 1000 et doit etre multiple de 5) : ");
+    scanf("%d",&montant);
+}
+
+int montant_valide(int montant){
+
+}
+
+int calcul_distribution(int montant){
+
+}
 
 int main (){
     int choix = 1;
@@ -13,12 +26,17 @@ int main (){
         afficher_menu();
         printf("Votre choix : ");
         scanf("%d",&choix);
-        if (choix != 1 || choix != 2){
+        if (choix == 1 ){
+            int montant = 0;
+            montant = saisir_montant();
+        }
+        else if (choix == 2){
+            return 0;
+        }
+        else{
             printf("Choississez entre 1 et 2\n");
             main();
         }
     }
-    
-
     return 0;
 }
