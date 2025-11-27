bb31d027ef82e9fb923db5ca7c907ebcf3199240
Mauriac <anatole.mauriac@gmail.com>
2025-11-27 17:45:09 +0100

    calcul de distribution

--- tp4/exo2.c
+++ tp4/exo2.c
@@ -17,6 +17,7 @@ int montant_valide(int montant){
     int oui = 0;
     if (montant<5 || montant >1000 || montant%5 != 0){
         oui = 0;
+        printf("Montant invalide !! (il doit etre compris entre 5 est 1000 et doit etre multiple de 5)\n");
     }
     else{
         oui = 1;
@@ -25,6 +26,24 @@ int montant_valide(int montant){
 }
 
 void calcul_distribution(int montant){
+    int cinq = 0;
+    int dix = 0;
+    int vingt = 0;
+    int cinqante = 0;
+
+    cinqante = montant/50;
+    montant -= 50*cinqante;
+    vingt = montant/20;
+    montant -= 20*vingt;
+    dix = montant/10;
+    montant -= 10*dix;
+    cinq = montant/5;
+        
+    printf("Billets distribues : \n");
+    printf("%d billets de 50\n",cinqante);
+    printf("%d billets de 20\n",vingt);
+    printf("%d billets de 10\n",dix);
+    printf("%d billets de 5\n",cinq);
 
 }
 
@@ -36,12 +55,12 @@ int main (){
         scanf("%d",&choix);
         if (choix == 1 ){
             int montant = 0;
-            montant = saisir_montant();
             int oui = 0;
             while (oui == 0){
+                montant = saisir_montant();
                 oui = montant_valide(montant);
             }
-            
+            calcul_distribution(montant);
         }
         else if (choix == 2){
             return 0;
