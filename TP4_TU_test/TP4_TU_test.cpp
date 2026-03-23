c8bb8ad00a1f6aaa380457e1d3a484c550624f45
Mauriac <anatole.mauriac@gmail.com>
2026-03-23 14:00:45 +0100

    test fonction max2

--- TP4_TU_test/TP4_TU_test.cpp
+++ TP4_TU_test/TP4_TU_test.cpp
@@ -13,29 +13,24 @@ namespace TP4TUtest
 	{
 	public:
 		
-		TEST_METHOD(TestEstPair)
+		TEST_METHOD(TestMax)
 		{
-			Assert::AreEqual(estPair(4), 1);
+			Assert::AreEqual(max2(5,3), 5);
 		}
 
-		TEST_METHOD(TestEstPair2)
+		TEST_METHOD(TestMax2)
 		{
-			Assert::AreEqual(estPair(7), 0);
+			Assert::AreEqual(max2(3, 5), 5);
 		}
 
-		TEST_METHOD(TestEstPair3)
+		TEST_METHOD(TestMax3)
 		{
-			Assert::AreEqual(estPair(0), 1);
+			Assert::AreEqual(max2(-1, -8), -1);
 		}
 
-		TEST_METHOD(TestEstPair4)
+		TEST_METHOD(TestMax4)
 		{
-			Assert::AreEqual(estPair(-2), 1);
-		}
-
-		TEST_METHOD(TestEstPair5)
-		{
-			Assert::AreEqual(estPair(-3), 0);
+			Assert::AreEqual(max2(4, 4), 4);
 		}
 	};
 }
