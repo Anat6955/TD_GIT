beb37fef9229aa5d49ff74ae8216ab9eb605a8ff
Mauriac <anatole.mauriac@gmail.com>
2026-03-23 13:54:17 +0100

    test est paire

--- TP4_TU_test/TP4_TU_test.cpp
+++ TP4_TU_test/TP4_TU_test.cpp
@@ -15,12 +15,27 @@ namespace TP4TUtest
 		
 		TEST_METHOD(TestEstPair)
 		{
-			Assert::AreEqual(estPair(6), 1);
+			Assert::AreEqual(estPair(4), 1);
 		}
 
 		TEST_METHOD(TestEstPair2)
 		{
 			Assert::AreEqual(estPair(7), 0);
+		}
+
+		TEST_METHOD(TestEstPair3)
+		{
+			Assert::AreEqual(estPair(0), 1);
+		}
+
+		TEST_METHOD(TestEstPair4)
+		{
+			Assert::AreEqual(estPair(-2), 1);
+		}
+
+		TEST_METHOD(TestEstPair5)
+		{
+			Assert::AreEqual(estPair(-3), 0);
 		}
 	};
 }
